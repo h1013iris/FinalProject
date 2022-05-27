4ebe3d174ab590cdef1e1962a80bd7e02d79b023
TaeYeon3 <taeyeon1374@gmail.com>
2022-05-27 09:17:58 +0900

    2022.05.27 taeyeon

--- src/main/java/com/uni/spring/calender/controller/CalenderController.java
+++ src/main/java/com/uni/spring/calender/controller/CalenderController.java
@@ -7,30 +7,35 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.uni.spring.calender.model.dto.Calender;
 import com.uni.spring.calender.model.dto.DateData;
+import com.uni.spring.calender.model.service.CalenderService;
+import com.uni.spring.member.model.dto.Member;
 
 @Controller
 public class CalenderController {
 	
-	//@Autowired
-	//public CalenderService calenderService;
+	@Autowired
+	public CalenderService calenderService;
 
 	@RequestMapping(value = "calendar.do", method = RequestMethod.GET)
 	public String calendar(Model model, HttpServletRequest request, DateData dateData){
 		
 		Calendar cal = Calendar.getInstance();
 		DateData calendarData;
-		//검색 날짜
+		
+		//검색 날짜(첫 시작은 오늘날짜이다. 이후 화살표표시 클릭시 이동할 검색날짜로 된다)
 		if(dateData.getDate().equals("")&&dateData.getMonth().equals("")){
 			dateData = new DateData(String.valueOf(cal.get(Calendar.YEAR)),String.valueOf(cal.get(Calendar.MONTH)),String.valueOf(cal.get(Calendar.DATE)),null);
 		}
+		
 		//검색 날짜 end
-
 		Map<String, Integer> today_info =  dateData.today_info(dateData);
 		List<DateData> dateList = new ArrayList<DateData>();
 		
@@ -73,5 +78,19 @@ public class CalenderController {
 	@RequestMapping(value = "enrollFormCalender.do")
 	public String enrollFormCalender() {
 		return "calender/calenderEnrollForm";
+	}
+	
+	@RequestMapping(value = "insertCalender.do", method = RequestMethod.POST)
+	public String insertCalender(Calender calender, String startTime, String endTime) {
+		System.out.println("controller 캘린더 insert ==> "+calender);
+		System.out.println("시작 시간 ==> "+startTime+" 종료시간 ==> " +endTime);
+		System.out.println(calender.getOneday());
+		if(calender.getOneday() == null) {
+			System.out.println("들어와라");
+			calenderService.insertCalender(calender, startTime, endTime);
+		}else {
+			calenderService.insertCalender(calender);
+		}
+		return "redirect:calendar.do";
 	}
 }
--- src/main/java/com/uni/spring/calender/model/service/CalenderService.java
+++ src/main/java/com/uni/spring/calender/model/service/CalenderService.java
@@ -1,5 +1,12 @@
 package com.uni.spring.calender.model.service;
 
+import com.uni.spring.calender.model.dto.Calender;
+
 public interface CalenderService {
 
+	void insertCalender(Calender calender);
+
+	void insertCalender(Calender calender, String startTime, String endTime);
+
+	
 }
