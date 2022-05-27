4c27217cb6e17b2423700bd8d46bfae3cb705fee
imcha <imcha@LAPTOP-G9MDQ88M>
2022-05-27 09:06:43 +0900

    2022/05/27

--- src/main/java/com/uni/spring/approval/model/dto/BusDraftForm.java
+++ src/main/java/com/uni/spring/approval/model/dto/BusDraftForm.java
@@ -13,6 +13,7 @@ public class BusDraftForm { // 업무 기안서
 
 	private int busDftNo; // 업무 기안서 번호
 	private int docNo; // 문서 번호
+	private int docTypeNo; // 문서 서식 번호
 	private String dftTitle; // 기안서 제목
 	private int drafter; // 기안자
 	private Timestamp dftDate; // 기안일
