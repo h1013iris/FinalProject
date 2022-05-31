981620c71b866ea7f38eaa1c9b97819d90617b61
ROHHANSEO <sola4441@gmail.com>
2022-05-31 09:23:39 +0900

    0531커밋

---
+++ src/main/java/com/uni/spring/admin/model/dto/Department.java
@@ -0,0 +1,18 @@
+package com.uni.spring.admin.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class Department {
+
+	private int departmentNo;
+	private String departmentTitle;
+}
